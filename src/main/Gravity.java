87a35ecd10536859225d1a566be12d353f19d1e0
Amrit Agarwal <54416489+sksketer@users.noreply.github.com>
2020-10-12 18:39:14 +0530

    Update Gravity.java

--- src/main/Gravity.java
+++ src/main/Gravity.java
@@ -1,16 +1,11 @@
-/*
- * @author Divyansh Bhardwaj
- * @version 1.0
- * */
 package main;
 
 import java.util.Scanner;
-// uncomment the above line to use input in your program.
 
 public class Gravity {
 	// TODO 1: Create the main method
 	public static void main(String[] args) {
-		double g= 9.8;
+		public static final double g= 9.8;
 		Scanner myTime = new Scanner(System.in);
 		double time= myTime.nextDouble();
 		double speed;
